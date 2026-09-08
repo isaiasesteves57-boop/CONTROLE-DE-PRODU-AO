957d48bc30ac46b7a782e760bcf510c0b2456021
isaiasesteves57-boop <isaiasesteves57@gmail.com>
2026-09-07 23:24:27 -0300

    Delete js/ordem-producao.js

--- js/ordem-producao.js
+++
@@ -1,9 +0,0 @@
-/* Módulo OP - FURACAO APP 56 */
-window.OPService={
- criar:function(op){
-  const lista=JSON.parse(localStorage.getItem('cp_ordens')||'[]');
-  lista.push({...op,criada:new Date().toISOString()});
-  localStorage.setItem('cp_ordens',JSON.stringify(lista));
- },
- buscar:function(numero){return (JSON.parse(localStorage.getItem('cp_ordens')||'[]')).filter(x=>String(x.op)===String(numero));}
-};
