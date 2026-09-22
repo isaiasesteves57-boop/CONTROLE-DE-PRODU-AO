7d4a994cc4a5255ff5a1e1f36b3a8c5c3e993156
isaiasesteves57-boop <isaiasesteves57@gmail.com>
2026-09-22 19:25:03 -0300

    Update firebase-config.js

--- shared/firebase-config.js
+++ shared/firebase-config.js
@@ -1,10 +1,13 @@
 // Reutiliza o mesmo projeto Firebase do Gestor 2026.
-// A chave pública do Firebase não substitui as regras de segurança.
 const GESTOR_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDrD-5L3twspJzadO4phOGHpF4L13WIJ2U",
+  apiKey: "SUA_API_KEY",
   authDomain: "gestor-de-banca-412d7.firebaseapp.com",
   projectId: "gestor-de-banca-412d7",
   storageBucket: "gestor-de-banca-412d7.firebasestorage.app",
-  messagingSenderId: "958380148833",
-  appId: "1:958380148833:web:57dca704b755a3227d0220"
+  messagingSenderId: "SEU_ID",
+  appId: "SEU_APP_ID"
 };
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(GESTOR_FIREBASE_CONFIG);
+}
