a4dc2a35b11970d3adc458e117e2d19d0f0c7828
isaiasesteves57-boop <isaiasesteves57@gmail.com>
2026-09-22 21:52:23 -0300

    Update firebase-config.js

--- admin/firebase-config.js
+++ admin/firebase-config.js
@@ -1,1 +1,9 @@
-
+ Configuração do Firebase do seu aplicativo web
+const firebaseConfig = { 
+  apiKey : "AIzaSyDrD-5L3twspJzadO4phOGHpF4L13WIJ2U" , 
+  authDomain : "gestor-de-banca-412d7.firebaseapp.com" , 
+  projectId : "gestor-de-banca-412d7" , 
+  bucket de armazenamento : "gestor-de-banca-412d7.firebasestorage.app" , 
+  messagingSenderId : " 958380148833" 
+  appId : "1:958380148833:web:57dca704b755a3227d0220" 
+};
